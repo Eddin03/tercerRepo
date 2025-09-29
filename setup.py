cfe8b84169a6583a85df9d2446b15927059fc810
Eddin Martinez <auxiliar.desarrollo@grupoess.com>
2025-09-29 09:37:21 -0500

    Mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paquetePlatzi",             # Nombre del paquete
+    version="0.1.0",                  # Versión inicial
+    packages=find_packages(),         # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Eddin Martinez",           # Tu nombre
+    author_email="martinezariaseddin@gmail.com",   # Tu correo electrónico
+    url="https://github.com/Eddin03/tercerRepo",  # URL del proyecto
+)
